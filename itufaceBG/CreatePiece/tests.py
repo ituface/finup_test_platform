3db56180335f6d17f0f3998b855a277655e8d620
ituface <ituface@163.com>
2019-07-22 10:52:37 +0000

    firs

--- itufaceBG/CreatePiece/tests.py
+++ itufaceBG/CreatePiece/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from django.http import HttpRequest
+from CreatePiece.views import add_piece_api
+
+
+class AddPieceApiTest(TestCase):
+
+
+    def test_add_piece_api_post_request(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['name']='测试一'
+        request.POST['mobile']='18329008888'
+        request.POST['status']='PUSH_TO_IRON'
+
+        response=add_piece_api(request)
+
+        self.assertIn('造件已完成',response.content.decode())
+
+
+
