7b2ab75c4dfbe70307d38b4cac4b3ec8ffeb3b50
ituface <ituface@163.com>
2019-08-27 09:41:13 +0000

    firs

---
+++ itufaceBG/AppManage/middleware.py
@@ -0,0 +1,15 @@
+import traceback
+import logging
+
+
+
+class OnlineMiddlware(object):
+    def __init__(self,get_response):
+        self.get_response=get_response
+
+    def __call__(self, *args, **kwargs):
+        try:
+         respose=self.get_response(*args,**kwargs)
+        except Exception as e:
+            logging.error('系统报错',traceback.print_exc())
+        return respose
