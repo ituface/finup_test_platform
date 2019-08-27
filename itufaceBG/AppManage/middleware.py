c361faa59f732391e0f3b466d070f706045b6c59
ituface <ituface@163.com>
2019-08-27 11:13:30 +0000

    firs

--- itufaceBG/AppManage/middleware.py
+++ itufaceBG/AppManage/middleware.py
@@ -3,17 +3,17 @@ import logging
 
 from django.shortcuts import HttpResponse
 
-class OnlineMiddlware(object):
-    def __init__(self,get_response):
-        self.get_response=get_response
-
-    def __call__(self, *args, **kwargs):
-        try:
-         respose=self.get_response(*args, **kwargs)
-        except Exception as e:
-            print('系统报错')
-            return HttpResponse('fffff')
-        return respose
+# class OnlineMiddlware(object):
+#     def __init__(self,get_response):
+#         self.get_response=get_response
+#
+#     def __call__(self, *args, **kwargs):
+#         try:
+#          respose=self.get_response(*args, **kwargs)
+#         except Exception as e:
+#             print('系统报错')
+#             return HttpResponse('fffff')
+#         return respose
 
 
 
