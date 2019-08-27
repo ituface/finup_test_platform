1aba6628f159923d43d85a84e10baf9f67d1cae3
ituface <ituface@163.com>
2019-08-27 09:25:48 +0000

    firs

--- itufaceBG/itufaceBG/settings.py
+++ itufaceBG/itufaceBG/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%_q%b)%pte+@vo!+r^0f8(mgk%p+ptl(jj_78#o&_mv$hxourg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 # Application definition
 
