ca7645cddea3635c08bb30d60246d357730987a7
ituface <ituface@163.com>
2019-08-27 11:17:04 +0000

    firs

--- itufaceBG/AppManage/middleware.py
+++ itufaceBG/AppManage/middleware.py
@@ -16,6 +16,30 @@ from django.shortcuts import HttpResponse
 #         return respose
 
 
+class TestMiddleware(object):
+    '''中间件类'''
+
+    def __init__(self):
+        '''服务器重启之后，接收第一个请求时调用(只会调用一次)'''
+        print('----init----')
+
+    # 中间件函数。(用到哪个函数写哪个，不需要全写)
+    def process_request(self, request):
+        '''产生request对象之后，url匹配之前调用'''
+        print('----process_request----')
+        # return HttpResponse('process_request')  # 默认放行,不拦截请求。
+
+    def process_view(self, request, view_func, *view_args, **view_kwargs):
+        '''url匹配之后，视图函数调用之前调用'''
+        print('----process_view----')
+        # view_func: url匹配到的视图函数。
+        return HttpResponse('process_view')  # return HttpResponse对象,表示拦截,直接执行process_response函数。
+
+    def process_response(self, request, response):
+        '''视图函数调用之后，response返回浏览器之前'''
+        print('----process_response----')
+        return response  # 一般会返回响应。
+
 
 class ExceptionTestMiddleware(object):
     # 如果注册多个process_exception函数，那么函数的执行顺序与注册的顺序相反。(其他中间件函数与注册顺序一致)
