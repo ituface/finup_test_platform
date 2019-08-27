64fb714753d23a8ffb04de1996ec05994343b298
ituface <ituface@163.com>
2019-08-27 09:41:53 +0000

    firs

--- itufaceBG/itufaceBG/settings.py
+++ itufaceBG/itufaceBG/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'AppManage.middleware.OnlineMiddleware',
+    'AppManage.middleware.OnlineMiddlware',
 ]
 
 ROOT_URLCONF = 'itufaceBG.urls'
