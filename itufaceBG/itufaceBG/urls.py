58a2016f7f3d679c2235d4e67446fafda25922ad
ituface <ituface@163.com>
2019-08-28 08:02:43 +0000

    firs

--- itufaceBG/itufaceBG/urls.py
+++ itufaceBG/itufaceBG/urls.py
@@ -16,30 +16,30 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from AppManage import views
-from AppApi import  views as apiview
+from AppApi import views as apiview
 from CreatePiece import views as pieceview
-
+from itufaceBG import settings
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', admin.site.urls),
-    url(r'app-list', views.app_list,name='app-list'),
-    url(r'^del_app_list/', views.del_app_list,name='del_app_list'),
-    url(r'add-app/', views.add_app,name='add-app'),
+    url(r'app-list', views.app_list, name='app-list'),
+    url(r'^del_app_list/', views.del_app_list, name='del_app_list'),
+    url(r'add-app/', views.add_app, name='add-app'),
     url(r'^login/', views.index),
-    url(r'update-app/', views.update_app,name='update-app'),
-    url(r'qr-ios-download/',views.qr_ios_download),
-    url(r'^index/$',apiview.index),
-    url(r'welcome',apiview.welcome),
-    url(r'piece-list',pieceview.piece_list,name='piece-list'),
-    url(r'^add-piece',pieceview.add_piece),
-    url(r'api-add-piece',pieceview.add_piece_api,name='api-add-piece'),
-    url(r'app-product',pieceview.app_product),
-    url(r'add-product',pieceview.add_product),
-    url(r'del_product_list',pieceview.del_product_list,name='del_product_list'),
-    url(r'del_piece_list',pieceview.del_piece_list,name='del_piece_list'),
-    url(r'finup_lottery',views.finup_lottery,name='finup_lottery'),
-    url(r'search-piece/',pieceview.search_piece,name='search-piece'),
-    url(r'supplement-materials',pieceview.first_second_supplement,name='supplement-materials'),
-    url(r'test',pieceview.django_test)
-
+    url(r'update-app/', views.update_app, name='update-app'),
+    url(r'qr-ios-download/', views.qr_ios_download),
+    url(r'^index/$', apiview.index),
+    url(r'welcome', apiview.welcome),
+    url(r'piece-list', pieceview.piece_list, name='piece-list'),
+    url(r'^add-piece', pieceview.add_piece),
+    url(r'api-add-piece', pieceview.add_piece_api, name='api-add-piece'),
+    url(r'app-product', pieceview.app_product),
+    url(r'add-product', pieceview.add_product),
+    url(r'del_product_list', pieceview.del_product_list, name='del_product_list'),
+    url(r'del_piece_list', pieceview.del_piece_list, name='del_piece_list'),
+    url(r'finup_lottery', views.finup_lottery, name='finup_lottery'),
+    url(r'search-piece/', pieceview.search_piece, name='search-piece'),
+    url(r'supplement-materials', pieceview.first_second_supplement, name='supplement-materials'),
+    url(r'test', pieceview.django_test)
 
 ]
